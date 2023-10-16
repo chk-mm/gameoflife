86c4810e7afbb452622ea27d48198fa29cd5cd0d
Chonnakarn Rungjaroenporn <chonnak.rung@gmail.com>
2023-10-16 10:55:39 +0700

    create the baord

---
+++ board.py
@@ -0,0 +1,17 @@
+class Board:
+    def __init__(self,width=3,height=3):
+        self.board = [[False]*3,
+                      [False]*3,
+                      [False]*3]
+
+    def as_str(self):
+        rv = ''
+        for row in self.board:
+            rv += ''.join('.0'[c] for c in row)
+            rv += '\n'
+        return rv
+
+
+
+test = Board()
+print(test.as_str())
